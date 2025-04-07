c451222f328b37cc9fb66837a7c7d42add47e803
Roger Braggio <rogerbraggio96@gmail.com>
2025-04-07 10:40:25 -0300

    Configuração inicial
    Configuração inicial de uma API usando o FastAPI.

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# 127.0.0.1:8000/
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+# 127.0.0.1:8000/teste
+@app.get('/teste')
+async def funcaoteste():
+    return {"teste": "deu certo"}
